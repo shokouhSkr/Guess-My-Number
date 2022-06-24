6f775267641246d9041fbc236bb5950a0737c376
shokouhSkr <shokouhskr@gmail.com>
2022-06-24 14:29:17 +0430

    secret number logic added

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const secretNumber = Math.trunc(Math.random() * 50) + 1;
+
+document.querySelector("#secretNumber").textContent = secretNumber;
+
+document.querySelector("#check").addEventListener("click", function () {
+  const guessNumber = +document.querySelector("#guess").value;
+  // console.log(guessNumber, typeof guessNumber);
+
+  if (!guessNumber) {
+    document.querySelector("#message").textContent = "⛔ No Number!";
+  } else if (guessNumber === secretNumber) {
+    document.querySelector("#message").textContent = "🎉 Correct Number";
+  } else if (
+    (guessNumber > secretNumber && guessNumber - secretNumber < 5) ||
+    (secretNumber > guessNumber && secretNumber - guessNumber < 5)
+  ) {
+    document.querySelector("#message").textContent = "😍 So Close";
+  } else if (guessNumber > secretNumber) {
+    document.querySelector("#message").textContent = "📈 Too High!";
+  } else if (secretNumber > guessNumber) {
+    document.querySelector("#message").textContent = "📉 Too Low!";
+  }
+});
